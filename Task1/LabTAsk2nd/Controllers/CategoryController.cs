25be48aef207be4ede58967850bf2959a937066b
Atia Rasida Silvia <atiasilvia0@gmail.com>
2023-10-20 20:46:39 +0600

    Task1 done

---
+++ Task1/LabTAsk2nd/Controllers/CategoryController.cs
@@ -0,0 +1,71 @@
+﻿using LabTAsk2nd.Ef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTAsk2nd.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Caegory data)
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+            db.Caegories.Add(data);
+            db.SaveChanges();
+            return RedirectToAction("List");
+
+        }
+
+        public ActionResult List()
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+            var data = db.Caegories.ToList();
+            return View(data);
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+
+            FoodPandaEntities db = new FoodPandaEntities();
+            var data = db.Caegories.Find(ID);
+            var data1 = (from d in db.Caegories where d.ID == ID select d).SingleOrDefault();
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Caegory caegory)
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+            var data = db.Caegories.Find(caegory.ID);
+            data.Name = caegory.Name;
+            
+            db.SaveChanges();
+            return RedirectToAction("List");
+
+        }
+
+        public ActionResult Delete(int ID)
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+
+            var data = db.Caegories.Find(ID);
+            db.Caegories.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("List");
+
+        }
+    }
+}
---
+++ Task1/LabTAsk2nd/Controllers/ProductController.cs
@@ -0,0 +1,80 @@
+﻿using LabTAsk2nd.Ef;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTAsk2nd.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult ProductList()
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+            return View(db.Products.ToList());
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product data)
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+            db.Products.Add(data);
+            db.SaveChanges();
+            return RedirectToAction("List");
+
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int ID)
+        {
+
+            FoodPandaEntities db = new FoodPandaEntities();
+            var data = db.Products.Find(ID);
+            var data1 = (from d in db.Products where d.ID == ID select d).SingleOrDefault();  
+            return View(data);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Product product)
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+            var data = db.Products.Find(product.ID);
+            data.Name = product.Name;
+            data.Price = product.Price;
+            data.CaegoriesID = product.CaegoriesID;
+            db.SaveChanges();
+            return RedirectToAction("List");
+
+        }
+
+        public ActionResult List()
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+            var data = db.Products.ToList();
+            return View(data);
+           
+        }
+
+
+        public ActionResult Delete(int ID)
+        {
+            FoodPandaEntities db = new FoodPandaEntities();
+         
+            var data = db.Products.Find(ID);
+            db.Products.Remove(data);
+            db.SaveChanges();
+            return RedirectToAction("List");
+
+        }
+
+
+    }
+}
